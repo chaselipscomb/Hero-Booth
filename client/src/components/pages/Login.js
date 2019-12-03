e56d129b685f3244b5f7fbbe95ab740b4ae24e29
William Mao <willmao87@gmail.com>
2019-12-03 10:01:11 -0800

    will

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { Component } from "react";
+import Form from 'react-bootstrap/Form'
+import "../pages/Login.css"
+import { Auth } from "aws-amplify";
+import Button from 'react-bootstrap/Button'
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
 
-function Login() {
-  return (
-    <React.Fragment>
-      <div>Login</div>
-    </React.Fragment>
-  )
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+  
+    try {
+      await Auth.signIn(this.state.email, this.state.password);
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
+    }
+  }
+  
+  render() {
+    return (
+      <div className="Login">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="email" bsSize="large">
+            <Form.Control
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="password" bsSize="large">
+            <Form.Control
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+            />
+          </Form.Group>
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >
+            Login
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
-export default Login;
+
